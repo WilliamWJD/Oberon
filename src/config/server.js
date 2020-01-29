8022778308a3d3c3a6cdd3693d52c122b63d0db6
williamwjd <william.jose.wjd@gmail.com>
2020-01-29 14:46:35 -0300

    Middleware para controlar paginação

--- src/config/server.js
+++ src/config/server.js
@@ -1,5 +1,6 @@
 const bodyParser=require('body-parser')
 const express=require('express')
+const queryParser=require('express-query-int')
 require('dotenv').config()
 
 const allowCors=require('../config/cors')
@@ -11,6 +12,7 @@ const server=express()
 server.use(bodyParser.urlencoded({extended:true}))
 server.use(bodyParser.json())
 server.use(allowCors)
+server.use(queryParser())
 
 server.listen(port,()=>{
     console.log(`Servidor online`)
