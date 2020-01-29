4943566f55555e9d77f91c6dad62d5e746ce5276
williamwjd <william.jose.wjd@gmail.com>
2020-01-29 14:05:18 -0300

    Criando rota para retornar total de registros no banco de dados

--- src/app/billingCycle/billingCycleServer.js
+++ src/app/billingCycle/billingCycleServer.js
@@ -3,4 +3,14 @@ const BillingCycle=require('./billingCycle')
 BillingCycle.methods(['get','post','put','delete'])
 BillingCycle.updateOptions({new: true, runValidators:true})
 
+BillingCycle.route('count',(req,res,nex)=>{
+    BillingCycle.count((error, value)=>{
+        if(error){
+            res.status(500).json({errors:[error]})
+        }else{
+            res.json({value})
+        }
+    })
+})
+
 module.exports=BillingCycle
