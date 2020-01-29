43c7ccc29a43f8bb894ae85f7fa3e20499342468
williamwjd <william.jose.wjd@gmail.com>
2020-01-29 14:41:35 -0300

    Criando middleware para o CORS

--- src/config/server.js
+++ src/config/server.js
@@ -2,12 +2,15 @@ const bodyParser=require('body-parser')
 const express=require('express')
 require('dotenv').config()
 
+const allowCors=require('../config/cors')
+
 const port=process.env.PORT_SERVER
 
 const server=express()
 
 server.use(bodyParser.urlencoded({extended:true}))
 server.use(bodyParser.json())
+server.use(allowCors)
 
 server.listen(port,()=>{
     console.log(`Servidor online`)
