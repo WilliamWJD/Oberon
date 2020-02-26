3ea2eb7f0b18937e8d68cbfdf02e7e13f1774c80
williamwjd <william.jose.wjd@gmail.com>
2020-02-26 11:00:11 -0300

    criando sidebar menu

--- frontend/src/Main/app.jsx
+++ frontend/src/Main/app.jsx
@@ -2,9 +2,11 @@ import '../common/template/dependencies'
 import React from 'react'
 
 import Header from '../common/template/header'
+import SideBar from '../common/template/sidebar'
 
 export default props=>(
     <div>
         <Header/>
+        <SideBar/>
     </div>
 )
