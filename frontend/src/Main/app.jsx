d44f85450708dfc4c411e0849bd2ebc1078ea70b
williamwjd <william.jose.wjd@gmail.com>
2020-02-26 10:11:56 -0300

    Implementando dependencias do projeto

--- frontend/src/Main/app.jsx
+++ frontend/src/Main/app.jsx
@@ -1,11 +1,8 @@
+import '../common/template/dependencies'
 import React from 'react'
 
-const teste=()=>{
-    return(
-        <div>
-            <h1>asdadsa</h1>
-        </div>
-    )
-}
-
-export default teste
+export default props=>(
+    <div className='wrapper'>
+        <h1>App</h1>
+    </div>
+)
