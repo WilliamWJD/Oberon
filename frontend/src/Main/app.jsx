8bfe84b0c8d8ea270a7943b7168c50269d9439b8
williamwjd <william.jose.wjd@gmail.com>
2020-02-26 11:06:11 -0300

    Criando o rodape

--- frontend/src/Main/app.jsx
+++ frontend/src/Main/app.jsx
@@ -3,10 +3,15 @@ import React from 'react'
 
 import Header from '../common/template/header'
 import SideBar from '../common/template/sidebar'
+import Footer from '../common/template/footer'
 
 export default props=>(
     <div>
         <Header/>
         <SideBar/>
+        <div className="content-wrapper">
+            <h1>Conteudo</h1>
+        </div>
+        <Footer/>
     </div>
 )
---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props=>(
+    <footer className="main-footer">
+        <strong>
+            Copyright &copy; 2020
+            <a href="https://www.linkedin.com/in/william-jos%C3%A9-dias-641820148/" target="_blank"> William José Dias</a>
+        </strong>
+    </footer>
+)
