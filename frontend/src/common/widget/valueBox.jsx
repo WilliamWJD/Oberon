a303b37b7cbb2ed7c69b050ad7fd0af43a9eb3eb
williamwjd <william.jose.wjd@gmail.com>
2020-02-27 10:15:18 -0300

    Criando componente valueBox

---
+++ frontend/src/common/widget/valueBox.jsx
@@ -0,0 +1,17 @@
+import React,{Component} from 'react'
+import Grid from '../layoult/grid'
+
+export default props=>(
+    <Grid cols={props.cols}>
+        <div className={`small-box bg-${props.color}`}>
+            <div className="inner">
+                <h3>{props.value}</h3>
+                <p>{props.text}</p>
+            </div>
+            <div className="icon">
+                <i className={`fa fa-${props.icon}`}></i>
+            </div>
+        </div>
+    </Grid>
+)
+
