a8fb4f6c7325a3f597389186e3632f17f065da2a
williamwjd <william.jose.wjd@gmail.com>
2020-02-28 08:38:32 -0300

    corrigindo o service/api

--- frontend/src/dashbord/dashbordActions.js
+++ frontend/src/dashbord/dashbordActions.js
@@ -1,7 +1,7 @@
 import api from '../services/api'
 
 export function getSummary(){
-    const response = api.get('/api/billingCycles/summary')
+    const response = api.get('/billingCycles/summary')
     return {
         type: 'BILLING_SUMMARY_FETCHED',
         payload: response
