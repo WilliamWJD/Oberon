58078290fbc922b13a5b52dadc3a42c77657ea90
williamwjd <william.jose.wjd@gmail.com>
2020-02-28 08:32:42 -0300

    exibindo creditos, debitos e valor consilidado no dashbord

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './Main/app'
 
-import { createStore } from 'redux'
+import { applyMiddleware ,createStore } from 'redux'
 import { Provider } from 'react-redux'
+
+import promise from 'redux-promise'
 
 import reducers from './Main/reducers'
 
-const store=createStore(reducers)
+const store=applyMiddleware(promise)(createStore)(reducers)
 
 ReactDOM.render(
     <Provider store={store}>
