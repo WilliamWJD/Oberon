40572b80e4346f107f0dcb41b866a767a0b47159
williamwjd <william.jose.wjd@gmail.com>
2020-03-01 11:46:05 -0300

    Criando component tabHeader

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -5,6 +5,7 @@ import Content from '../common/template/content'
 import Tabs from '../common/tab/tabs'
 import TabsHeader from '../common/tab/tabsHeader'
 import TabsContent from '../common/tab/tabsContent'
+import TabHeader from '../common/tab/tabHeader'
 
 class BillingCycle extends Component{
     render(){
@@ -14,7 +15,26 @@ class BillingCycle extends Component{
                 <Content>
                     <Tabs>
                         <TabsHeader>
-
+                            <TabHeader
+                                label="Listar"
+                                icon="bars"
+                                target="tabList"
+                            />
+                            <TabHeader
+                                label="Incluir"
+                                icon="plus"
+                                target="tabCreate"
+                            />
+                            <TabHeader
+                                label="Alterar"
+                                icon="pencil"
+                                target="tabUpdate"
+                            />
+                            <TabHeader
+                                label="Excluir"
+                                icon="trash-o"
+                                target="tabDelete"
+                            />
                         </TabsHeader>
                         <TabsContent>
                             
---
+++ frontend/src/common/tab/tabHeader.jsx
@@ -0,0 +1,19 @@
+import React,{Component} from 'react'
+
+class TabHeader extends Component{
+    render(){
+        return(
+            <li>
+                <a 
+                    href="javascript:;" 
+                    data-toggle='tab' 
+                    data-target={this.props.target}
+                >
+                    <i className={`fa fa-${this.props.icon}`}></i>{this.props.label}
+                </a>
+            </li>
+        )
+    }
+}
+
+export default TabHeader
