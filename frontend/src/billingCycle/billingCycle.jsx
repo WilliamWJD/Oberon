87d98f59c88ea94905cf98ac401eb7379c01fac8
williamwjd <william.jose.wjd@gmail.com>
2020-03-02 09:37:08 -0300

    conectando o componente billingCycle dom o redux

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -1,4 +1,6 @@
 import React,{Component} from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
@@ -8,7 +10,14 @@ import TabsContent from '../common/tab/tabsContent'
 import TabHeader from '../common/tab/tabHeader'
 import TabContent from '../common/tab/tabContent'
 
+import { selectTab } from '../common/tab/tabActions'
+
 class BillingCycle extends Component{
+
+    componentWillMount(){
+        this.props.selectTab('tabList')
+    }
+
     render(){
         return(
             <div>
@@ -58,4 +67,7 @@ class BillingCycle extends Component{
     }   
 }
 
-export default BillingCycle
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({selectTab}, dispatch);
+
+export default connect(null, mapDispatchToProps)(BillingCycle)
