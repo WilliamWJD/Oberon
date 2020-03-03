2267d9787eef62ec49c7cb0542d2f1112dcb85b3
williamwjd <william.jose.wjd@gmail.com>
2020-03-03 20:13:22 -0300

    Field com componente personalizado

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -1,6 +1,8 @@
 import React,{Component} from 'react'
 import { reduxForm, Field } from 'redux-form'
 
+import labelAndInput from '../common/form/labelAndInput'
+
 class BillingCycleForm extends Component{
     render(){
         
@@ -9,9 +11,29 @@ class BillingCycleForm extends Component{
         return(
             <form role="form" onSubmit={handleSubmit}>
                 <div className="box-body">
-                    <Field name="name" component='input'/>
-                    <Field name="month" component='input'/>
-                    <Field name="year" component='input'/>
+                    <Field 
+                        name="name" 
+                        component={labelAndInput}
+                        label="Nome"
+                        cols='12 4'
+                        placeholder="Informe o nome"
+                    />
+                    <Field 
+                        name="month" 
+                        component={labelAndInput}
+                        type="number"
+                        label="Mês"
+                        cols='12 4'
+                        placeholder="Informe o mês"
+                    />
+                    <Field 
+                        name="year" 
+                        component={labelAndInput}
+                        type="number"
+                        label="Ano"
+                        cols="12 4"
+                        placeholder="Informe o ano"
+                    />
                 </div>
                 <div className="box-footer">
                     <button type="submit" className="btn btn-primary">Submit</button>
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL:"http://192.168.0.102:3333/api"
+    baseURL:"http://192.168.0.22:3333/api"
 })
 
 export default api
