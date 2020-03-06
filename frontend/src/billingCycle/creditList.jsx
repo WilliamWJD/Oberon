8e8646490ce59a67490fe257daaa96413c0ed76b
williamwjd <william.jose.wjd@gmail.com>
2020-03-06 11:11:11 -0300

    Adicionando e clonando creditos

--- frontend/src/billingCycle/creditList.jsx
+++ frontend/src/billingCycle/creditList.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
-import { Field } from 'redux-form'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { Field, arrayInsert } from 'redux-form'
 
 import Grid from '../common/layoult/grid'
 import Input from '../common/form/input'
 
 class CreditList extends Component {
+
+    add(index, item = {}){
+        if(!this.props.readOnly){
+            this.props.arrayInsert('billingCycleForm', 'credits', index, item)
+        }
+    }
 
     renderRows() {
         const list = this.props.list || []
@@ -26,7 +34,22 @@ class CreditList extends Component {
                         readOnly={this.props.readOnly}
                     />
                 </td>
-                <td></td>
+                <td className="table-actions">
+                    <button 
+                        type="button" 
+                        className="btn btn-success"
+                        onClick={()=>this.add(index + 1)}
+                    >
+                        <i className="fa fa-plus"></i>
+                    </button>
+                    <button 
+                        type="button" 
+                        className="btn btn-warning"
+                        onClick={()=>this.add(index + 1, item)}
+                    >
+                        <i className="fa fa-clone"></i>
+                    </button>
+                </td>
             </tr>
         ))
     }
@@ -54,4 +77,7 @@ class CreditList extends Component {
     }
 }
 
-export default CreditList
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({arrayInsert}, dispatch);
+
+export default  connect(null, mapDispatchToProps)(CreditList)
